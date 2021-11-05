10c080d4d3c9c599787a67ae573c8e815aee42ef
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2021-11-05 21:18:30 +0100

    Detecting Loops

--- app/app.js
+++ app/app.js
@@ -106,6 +106,10 @@ async function openDiagram(xml) {
     console.log("pairedElements:");
     console.log(pairedElements);
 
+    const classifiedElements = classifyElements(pairedElements);
+    console.log("classifiedElements:");
+    console.log(classifiedElements);
+
 
     function convertElements(e) {
       const fullElement = businessElements[e.id];
@@ -113,19 +117,40 @@ async function openDiagram(xml) {
       const outgoing = fullElement.element.outgoing.length;
 
       if (incoming == 1) {
-        return ({ id: e.id, type: e.type, sm: "split", outgoing: outgoing, uncertainty: 1, paired: false, pairid:"" });
+        /**
+         * TODO:
+         * classify attribut
+         * -> exclusives gateway nicht zwangsläufig xor block -> kann auch loop sein
+         * -> classify loop -> loop anfang und loop ende
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+        return ({ id: e.id, type: e.type, sm: "split", outgoing: outgoing, uncertainty: 1, paired: false, pairid:"", isLoop: false, classification: "" });
       } else {
-        return ({ id: e.id, type: e.type, sm: "merge", incoming: incoming, uncertainty: 1, paired: false, pairid:"" });
+        return ({ id: e.id, type: e.type, sm: "merge", incoming: incoming, uncertainty: 1, paired: false, pairid:"", isLoop: false, classification: "" });
       }
     };
 
     function pairElements(listsOfElements) {
       var pairedList = listsOfElements;
-      var unpairedElements = 1;
-      var counter = 0;
-      while (unpairedElements > 0 && counter < 1000) { //counter als absicherung damit kein endlosloop entsteht
+      //var unpairedElements = 1;
+      //var counter = 0;
 
+      for (let x = 0; x < listsOfElements.length; x++) {
+      //while (unpairedElements > 0 && counter < 1000) { //counter als absicherung damit kein endlosloop entsteht
         var filteredUnpairedList = pairedList.map(e => e.filter(m => m.paired != true));
+        if(filteredUnpairedList.length >= 0){
         for (let i = 0; i < filteredUnpairedList.length; i++) {
           const listOfElements = filteredUnpairedList[i];
           for (let j = 0; j < listOfElements.length; j++) {
@@ -135,38 +160,33 @@ async function openDiagram(xml) {
           }
         }
 
-        unpairedElements = 0;
+        /*unpairedElements = 0;
         for (let i = 0; i < filteredUnpairedList.length; i++) {
           unpairedElements += filteredUnpairedList[i].length;
           //console.log(filteredUnpairedList[i].length);
         }
         counter = counter + 1;
-        console.log(counter);
+        console.log(counter);*/
       }
+    }
       return (pairedList);
     }
 
     function checkIfSequence(unpairedElements, a, b) {
-      var check = 0;
+      var check = false;
       if (b != undefined) {
         for (let i = 0; i < unpairedElements.length; i++) {
           const elements = unpairedElements[i];
 
-          for (let j = 0; j < elements.length; j++) {
+          for (let j = 0; j < elements.length-1; j++) {
             //console.log(elements);
-            if ((elements[j].id == a.id && elements[j + 1].id != b.id) || !(a.sm == "split" && b.sm == "merge")) {
-              check += 1;
+            if ((elements[j].id == a.id && elements[j + 1].id == b.id) && (a.sm == "split" && b.sm == "merge")) {
+              check = true;
             }
           }
         }
-      } else if (a.sm != "split" && b == undefined) {
-        check = 1;
       }
-      if (check == 0) {
-        return (true);
-      } else {
-        return (false);
-      }
+      return check;
     }
 
     function setAttributes(elementArray, elem1, elem2) {
@@ -191,6 +211,51 @@ async function openDiagram(xml) {
         }
       }
       return newElementArray;
+    }
+
+    function classifyElements(elemArray) {
+      const idArray = elemArray.map(m => m.map(e => e.id));
+      var toclassifyArray = elemArray;
+      //console.log("idArray:");
+      //console.log(idArray);
+      const duplicates = idArray.map(e => e.filter((elem, index) => e.indexOf(elem) !== index));
+      //console.log("duplicates:");
+      //console.log(duplicates);
+      var duplicatesConverted = [];
+      for (let i = 0; i < duplicates.length; i++) {
+        duplicatesConverted = duplicatesConverted.concat(duplicates[i]);
+      }
+      console.log("duplicatesConverted:");
+      console.log(duplicatesConverted);
+      for (let i = 0; i < toclassifyArray.length; i++) {
+        for (let j = 0; j < toclassifyArray[i].length; j++) {
+          if(duplicatesConverted.includes(toclassifyArray[i][j].id)){
+            toclassifyArray[i][j].isLoop = true;
+            for (let x = 0; x < toclassifyArray.length; x++) {
+              for (let y = 0; y < toclassifyArray[x].length; y++) {
+                if(toclassifyArray[x][y].pairid == toclassifyArray[i][j].pairid) {
+                  toclassifyArray[x][y].isLoop = true;
+                }
+              }
+            }
+          }
+        }
+      }
+      for (let i = 0; i < toclassifyArray.length; i++) {
+        for (let j = 0; j < toclassifyArray[i].length; j++) {
+          const classifyElem = toclassifyArray[i][j];
+          if(classifyElem.isLoop) {
+            classifyElem.classification = "Loop";
+          } else if(classifyElem.type.includes("Exclusive")) {
+            classifyElem.classification = "XOR";
+          } else if(classifyElem.type.includes("Parallel")) {
+            classifyElem.classification = "AND"
+          } else if(classifyElem.type.includes("Inclusive")) {
+            classifyElem.classification = "OR"
+          }
+        }
+      }
+      return toclassifyArray;
     }
 
   })();
